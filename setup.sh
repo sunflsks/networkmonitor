e395b8f77b28a3b87394369fc5f6f087c57eafc3
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2023-11-20 16:25:03 -0600

    make sure setup script is run as root

--- setup.sh
+++ setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-sudo /usr/bin/qmicli --device=/dev/cdc-wdm0 --device-open-proxy --wds-start-network="ip-type=4,apn=TFDATA" --client-no-release-cid
-sudo ip link set dev wwan0 up
-sudo udhcpc -q -f -i wwan0
+if [ $UID -ne 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+/usr/bin/qmicli --device=/dev/cdc-wdm0 --device-open-proxy --wds-start-network="ip-type=4,apn=TFDATA" --client-no-release-cid
+ip link set dev wwan0 up
+udhcpc -q -f -i wwan0
