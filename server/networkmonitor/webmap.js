a792e93f4cf8132d2d61a4737033a530f1945b25
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2023-11-30 21:48:02 -0600

    add preliminary heatmap
    not a "true" heatmap, as it doesn't really do it by density but rather
    by value. also isn't really like a heatmap heatmap; hexbins are used
    instead

--- server/networkmonitor/webmap.js
+++ server/networkmonitor/webmap.js
@@ -1,27 +1,60 @@
-// Import the leaflet package
-var L = require('leaflet');
+require('leaflet');
+require('d3/dist/d3')
+require('d3-hexbin/build/d3-hexbin.min')
+require('./js/leaflet-d3')
 
 // Creates a leaflet map binded to an html <div> with id "map"
 // setView will set the initial map view to the location at coordinates
 // 13 represents the initial zoom level with higher values being more zoomed in
-var map = L.map('map').setView([43.659752, -79.378161], 20);
+var map = L.map('map', {center: [32.906950, -96.950270], zoom: 15})
 
 // Adds the basemap tiles to your web map
 // Additional providers are available at: https://leaflet-extras.github.io/leaflet-providers/preview/
-L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	subdomains: 'abcd',
-	maxZoom: 21
+var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; OSM Mapnik <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// Adds a popup marker to the webmap for GGL address
-L.circleMarker([43.659752, -79.378161]).addTo(map)
-	.bindPopup(
-		'MON 304<br>' + 
-		'Monetary Times Building<br>' +
-		'341 Victoria Street<br>' + 
-		'Toronto, Ontario, Canada<br>' +
-		'M5B 2K3<br><br>' + 
-		'Tel: 416-9795000 Ext. 5192'
-	)
-	.openPopup();
+var options = {
+    radius : 12,
+    opacity: 1,
+    duration: 500,
+	colorRange: [ '#660000', '#5af019']
+};
+
+var hexLayer = L.hexbinLayer(options).addTo(map)
+
+var rssiToColorValue = function(rssi) {
+	if (rssi > -65) { return 5; }
+	if (rssi > -75) { return 4; }
+	if (rssi > -85) { return 3; }
+	if (rssi > -95) { return 4; }
+	return 1;
+};
+
+hexLayer
+  .radiusRange([6, 11])
+  .lng(function(d) { return d[1]; })
+  .lat(function(d) { return d[0]; })
+  .colorValue(function(d, i) {
+	 	const rssi = d[0].o[2];
+		console.log(rssi)
+		return rssiToColorValue(rssi);
+ 	})
+  .radiusValue(function(d) { return d.length; });
+
+setTimeout(function() {
+	fetch('http://localhost:3000/data')
+	.then(response => response.json())
+	.then(data => {
+	  toadd = []
+	  data.forEach(function(d) {
+		if (d.latitude == null || d.longitude == null)
+		  return;
+		d.rssi = parseInt(d.rssi);
+		toadd.push([d.latitude, d.longitude, d.rssi]);
+	  });
+
+	  hexLayer.data(toadd).addTo(map);
+	});
+}, 10000);
