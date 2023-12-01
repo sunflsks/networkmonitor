2fc2dc2feb10b139443fa7b59dc2badf74b70b63
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2023-11-30 22:21:45 -0600

    add data express route

---
+++ server/networkmonitor/routes/data.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var sqlite = require('sqlite3');
+var router = express.Router();
+
+const DB_PATH="/Users/sunchipnacho/Source/networkmonitor/ping_results.db";
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // open sqlite database and query for latitude, longitude, and rssi. then send over as JSON
+  var db = new sqlite.Database(DB_PATH);
+  db.all("SELECT latitude, longitude, rssi FROM results", function(err, rows) {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error querying database");
+    } else {
+      res.json(rows);
+    }
+  });
+});
+
+module.exports = router;
