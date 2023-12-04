edb34bc05c0269604e8508a92f7217d9b44a0163
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2023-12-04 15:55:59 +0000

    add timestamp field

--- server/networkmonitor/routes/data.js
+++ server/networkmonitor/routes/data.js
@@ -2,17 +2,19 @@ var express = require('express');
 var sqlite = require('sqlite3');
 var router = express.Router();
 
-const DB_PATH="/usr/local/share/ping_results.db";
+const DB_PATH = "/usr/local/share/ping_results.db";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   // open sqlite database and query for latitude, longitude, and rssi. then send over as JSON
+  ts = new Date(parseInt(req.query.timestamp)) ?? new Date().setHours(new Date().getHours() - 2);
   var db = new sqlite.Database(DB_PATH);
-  db.all("SELECT latitude, longitude, rssi FROM results", function(err, rows) {
+  db.all("SELECT latitude, longitude, rssi, timestamp FROM results WHERE timestamp > ?", [ts.toISOString()], function (err, rows) {
     if (err) {
       console.log(err);
       res.status(500).send("Error querying database");
     } else {
+      console.log(`Sending ${rows.length} rows to ${req.ip}`);
       res.json(rows);
     }
   });
