ba4050ed5009988058dd162c66fd97032900ade2
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2023-12-04 16:23:30 +0000

    reformat + make webmap only pull new values from server

--- server/networkmonitor/webmap.js
+++ server/networkmonitor/webmap.js
@@ -6,7 +6,7 @@ require('./js/leaflet-d3')
 // Creates a leaflet map binded to an html <div> with id "map"
 // setView will set the initial map view to the location at coordinates
 // 13 represents the initial zoom level with higher values being more zoomed in
-var map = L.map('map', {center: [32.906950, -96.950270], zoom: 15})
+var map = L.map('map', { center: [32.906950, -96.950270], zoom: 15 })
 
 // Adds the basemap tiles to your web map
 // Additional providers are available at: https://leaflet-extras.github.io/leaflet-providers/preview/
@@ -16,15 +16,15 @@ var osm_mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 }).addTo(map);
 
 var options = {
-    radius : 12,
-    opacity: 1,
-    duration: 500,
-	colorRange: [ '#660000', '#5af019']
+	radius: 12,
+	opacity: 1,
+	duration: 500,
+	colorRange: ['#660000', '#5af019']
 };
 
 var hexLayer = L.hexbinLayer(options).addTo(map)
 
-var rssiToColorValue = function(rssi) {
+var rssiToColorValue = function (rssi) {
 	if (rssi > -65) { return 5; }
 	if (rssi > -75) { return 4; }
 	if (rssi > -85) { return 3; }
@@ -33,29 +33,34 @@ var rssiToColorValue = function(rssi) {
 };
 
 hexLayer
-  .radiusRange([6, 11])
-  .lng(function(d) { return d[1]; })
-  .lat(function(d) { return d[0]; })
-  .colorValue(function(d, i) {
-	 	const rssi = d[0].o[2];
+	.radiusRange([6, 11])
+	.lng(function (d) { return d[1]; })
+	.lat(function (d) { return d[0]; })
+	.colorValue(function (d, i) {
+		const rssi = d[0].o[2];
 		console.log(rssi)
 		return rssiToColorValue(rssi);
- 	})
-  .radiusValue(function(d) { return d.length; });
+	})
+	.radiusValue(function (d) { return d.length; });
 
-setTimeout(function() {
-	const data_url = location.origin + "/data";
+// Set initial date to beginning of time
+date = new Date(0);
+
+// Some values may be skipped every now and then; will need to fix this later
+setTimeout(function () {
+	const data_url = location.origin + `/data?timestamp=${date.getTime()}`;
 	fetch(data_url)
-	.then(response => response.json())
-	.then(data => {
-	  toadd = []
-	  data.forEach(function(d) {
-		if (d.latitude == null || d.longitude == null)
-		  return;
-		d.rssi = parseInt(d.rssi);
-		toadd.push([d.latitude, d.longitude, d.rssi]);
-	  });
+		.then(response => response.json())
+		.then(data => {
+			toadd = []
+			data.forEach(function (d) {
+				if (d.latitude == null || d.longitude == null)
+					return;
+				d.rssi = parseInt(d.rssi);
+				toadd.push([d.latitude, d.longitude, d.rssi]);
+			});
 
-	  hexLayer.data(toadd).addTo(map);
-	});
+			hexLayer.data(toadd).addTo(map);
+		});
+	date = new Date();
 }, 10000);
