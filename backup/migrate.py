527d78d0bed5709b614879758d5b1217a291a139
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2023-12-12 21:00:31 -0600

    add backup folder

---
+++ backup/migrate.py
@@ -0,0 +1,63 @@
+import sqlite3
+import sys
+import psycopg2
+from psycopg2 import sql
+from datetime import datetime
+
+# Function to parse and format timestamp
+def format_timestamp(iso_timestamp):
+    return datetime.fromisoformat(iso_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+# Function to extract and convert RSSI to integer
+def convert_rssi(rssi_str):
+    # Extract the numeric part and convert to integer
+    return int(rssi_str.split()[0])
+
+# Function to transform data
+def transform_data(row):
+    formatted_timestamp = format_timestamp(row[0])
+    # Convert RSSI to integer
+    rssi_int = convert_rssi(row[3])
+    # Convert packet_dropped to boolean
+    packet_dropped = row[4] == 1
+    # Convert latitude and longitude to point
+    coordinates = f"({row[5]}, {row[6]})"
+    return (formatted_timestamp,) + row[1:3] + (rssi_int,) + (packet_dropped, coordinates)
+
+
+# Connect to SQLite database
+sqlite_conn = sqlite3.connect('db.db')
+cursor = sqlite_conn.cursor()
+
+# Fetch data from SQLite database
+cursor.execute("SELECT timestamp, ip_address, latency, rssi, packet_dropped, latitude, longitude FROM results")
+sqlite_data = cursor.fetchall()
+
+# Close the SQLite connection
+sqlite_conn.close()
+
+# Transform data
+sqlite_data = [x for x in sqlite_data if x[5]]
+transformed_data = [transform_data(row) for row in sqlite_data]
+
+# Connect to PostgreSQL database
+postgres_conn = psycopg2.connect(
+    dbname="networkmonitor",
+    user="postgres",
+    password="postgres"
+)
+postgres_cursor = postgres_conn.cursor()
+
+# Insert data into PostgreSQL database
+insert_query = sql.SQL("""
+    INSERT INTO results (timestamp, ip_address, latency, rssi, packet_dropped, coordinates)
+    VALUES (%s, %s, %s, %s, %s, %s)
+""")
+postgres_cursor.executemany(insert_query, transformed_data)
+
+# Commit changes and close the PostgreSQL connection
+postgres_conn.commit()
+postgres_cursor.close()
+postgres_conn.close()
+
+print("Data migration completed successfully.")
