152014f47ed49b37588b6e54e1fef58e70401f4c
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2024-02-20 18:07:11 +0000

    use mmcli instead of manually enabling and connecting modem

--- device/setup/init.sh
+++ device/setup/init.sh
@@ -4,9 +4,7 @@ if [ $UID -ne 0 ]; then
     echo "Please run as root"
     exit 1
 fi
-# get rid of me sometime pls
-sleep 10
-qmicli -d /dev/cdc-wdm0 -E raw-ip
-qmicli --device=/dev/cdc-wdm0 --device-open-proxy --wds-start-network="ip-type=4,apn=TFDATA" --client-no-release-cid
-ip link set dev wwan0 up
+
+mmcli -m 0 --enable
+mmcli -m 0 --simple-connect="apn=TFDATA"
 udhcpc -q -f -i wwan0
