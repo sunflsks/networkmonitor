cacdaa0f2c5d74b21a0c69a0f7fa79da1f0c224f
sunchipnacho <22717474+sunflsks@users.noreply.github.com>
2024-04-01 23:20:07 +0100

    constants refactor

--- device/constants.py
+++ device/constants.py
@@ -1,9 +1,17 @@
+# Local paths
 QMICLI_GET_INFO = "/usr/local/lib/networkmonitor/qmicli-get-info"
-PING_ADDRESS = "8.8.8.8"
-LED_PIN = 17
-SERIAL_PATH = "/dev/ttyS0"
-INTERFACE = "wwan0"
-DEVICE = "test"
-SERVER_URL = "http://104.189.146.210:25566"
 SQLITE_DB_PATH = "/home/sunchipnacho/ping_results.db"
 LEDCTL = "/usr/local/lib/networkmonitor/ledctl"
+SERIAL_PATH = "/dev/ttyS0"
+
+# URLs/IP
+SERVER_URL = "http://104.189.146.210:25566"
+PING_ADDRESS = "8.8.8.8"
+
+# Interface/Cellular
+INTERFACE = "wwan0"
+
+# Internal constants
+LED_PIN = 17
+DEVICE = "rpi1"
+UPLOAD_INTERVAL = 20  # seconds
